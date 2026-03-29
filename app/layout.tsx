c038f0fd22070c4bd99dd651479862d77aa692f6
Michael Miller <mjm9189@gmail.com>
2026-03-29 02:23:53 -0700

    Add new icon, modify fonts, and customize theme colors

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,18 @@
-import { Geist, Geist_Mono, Inter } from "next/font/google"
+import { JetBrains_Mono, Inter } from "next/font/google"
 
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
 import { cn } from "@/lib/utils";
+import Head from "next/head";
+import { Button } from "@/components/ui/button";
+import { Moon, Sun } from "lucide-react";
 
-const inter = Inter({subsets:['latin'],variable:'--font-sans'})
+const fontSans = Inter({
+  subsets: ["latin"],
+  variable: "--font-sans",
+})
 
-const fontMono = Geist_Mono({
+const fontMono = JetBrains_Mono({
   subsets: ["latin"],
   variable: "--font-mono",
 })
@@ -20,8 +26,29 @@ export default function RootLayout({
     <html
       lang="en"
       suppressHydrationWarning
-      className={cn("antialiased", fontMono.variable, "font-sans", inter.variable)}
+      className={cn(
+        "antialiased",
+        fontMono.variable,
+        "font-sans",
+        fontSans.variable
+      )}
     >
+      <Head>
+        <link
+          rel="icon"
+          type="image/png"
+          href="/favicon-96x96.png"
+          sizes="96x96"
+        />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
       <body>
         <ThemeProvider>{children}</ThemeProvider>
       </body>
